3dbde053bc51874577c78eea99ab43bb517f2612
AdrianSzewczyk <as310811@student.polsl.pl>
2025-04-24 21:07:52 +0200

    repo

---
+++ WSPPcars/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WSPPcars;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnZarejestruj_Click(object sender, RoutedEventArgs e)
+    {
+        Rejestracja rejestracja = new Rejestracja();
+        rejestracja.ShowDialog();
+    }
+
+    private void btnZaloguj_Click(object sender, RoutedEventArgs e)
+    {
+        Logowanie logowanie = new Logowanie();
+        logowanie.ShowDialog();
+    }
+}
