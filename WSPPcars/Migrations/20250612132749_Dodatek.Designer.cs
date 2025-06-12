b726ac5e8de8b678adf743cba1e345fd56a73125
AdrianSzewczyk <as310811@student.polsl.pl>
2025-06-12 15:28:14 +0200

    migracjaDodatkowa

--- WSPPcars/Migrations/20250612132749_Dodatek.Designer.cs
+++ WSPPcars/Migrations/20250612132749_Dodatek.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WSPPCars.Models;
 
@@ -11,9 +12,11 @@ using WSPPCars.Models;
 namespace WSPPCars.Migrations
 {
     [DbContext(typeof(DbWsppcarsContext))]
-    partial class DbWsppcarsContextModelSnapshot : ModelSnapshot
+    [Migration("20250612132749_Dodatek")]
+    partial class Dodatek
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WSPPcars/Migrations/20250612132749_Dodatek.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WSPPCars.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dodatek : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Zdjecie",
+                table: "Pojazd_Sztuka",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "data_dodania",
+                table: "Ogloszenia",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Zdjecie",
+                table: "Pojazd_Sztuka");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "data_dodania",
+                table: "Ogloszenia",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
