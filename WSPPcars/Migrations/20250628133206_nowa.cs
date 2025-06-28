12d0008a3fb70bf8b1e311f87c8818ad9ac80c6e
chaosRobe <dominik.panasz@outlook.com>
2025-06-28 16:13:49 +0200

    poprawka

--- WSPPcars/Migrations/20250628133206_nowa.cs
+++ WSPPcars/Migrations/20250628133206_nowa.cs
@@ -10,13 +10,55 @@ namespace WSPPCars.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Rezerwacje.utworzona
+            migrationBuilder.DropColumn(
+                name: "utworzona",
+                table: "Rezerwacje");
 
+            migrationBuilder.AddColumn<DateTime>(
+                name: "utworzona",
+                table: "Rezerwacje",
+                type: "datetime2",
+                nullable: true); // lub false, jeśli wymagane
+
+            // Uzytkownicy.utworzony
+            migrationBuilder.DropColumn(
+                name: "utworzony",
+                table: "Uzytkownicy");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "utworzony",
+                table: "Uzytkownicy",
+                type: "datetime2",
+                nullable: true); // zmień na false, jeśli wymagane
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            // Rezerwacje.utworzona
+            migrationBuilder.DropColumn(
+                name: "utworzona",
+                table: "Rezerwacje");
 
+            migrationBuilder.AddColumn<byte[]>(
+                name: "utworzona",
+                table: "Rezerwacje",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            // Uzytkownicy.utworzony
+            migrationBuilder.DropColumn(
+                name: "utworzony",
+                table: "Uzytkownicy");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "utworzony",
+                table: "Uzytkownicy",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
         }
     }
 }
